ca10c55a8c796e49cfaa4b5769d79c8ac92bca08
Sajib <sajibcse14@gmail.com>
2024-10-23 00:56:03 +0600

    code added for sequelize close

--- src/Application.js
+++ src/Application.js
@@ -3,7 +3,8 @@ import routing from "./route/Routing.js";
 import { PORT, INITIAL_ROUTE } from "../resources/properties.js";
 
 class Application {
-  static start() {
+
+  start() {
     const app = express();
 
     app.use(INITIAL_ROUTE, routing.getRouter());
@@ -12,13 +13,30 @@ class Application {
       res.status(404).json({ message: "Not Found" });
     });
 
+    // Listen for termination signals
+    process.on("SIGINT", this.shutdown);
+    process.on("SIGTERM", this.shutdown);
+
     // Start server
     const port = process.env.PORT || PORT;
     app.listen(PORT, () => {
       console.log(`Server running on port ${port}`);
     });
   }
+
+  // Graceful shutdown function
+  async shutdown() {
+    try {
+      await sequelize.close(); // Close Sequelize connection
+      console.log("Database connection closed.");
+      process.exit(0); // Exit the process
+    } catch (error) {
+      console.error("Error closing the database connection:", error);
+      process.exit(1); // Exit with an error code
+    }
+  }
 }
 
 //start execution
-Application.start();
+const application = new Application();
+application.start();
